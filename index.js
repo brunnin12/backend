f14d917643fa2cd359b4cbf81a26d51507e072f2
Brunno <brunnopereira090@gmail.com>
2023-04-26 12:11:34 -0300

    Subindo pro Hub

---
+++ index.js
@@ -0,0 +1,32 @@
+
+    let valor_total = document.getElementById('valor_total')
+    let total = document.getElementById('total')
+
+    let qtd = []
+    let preco = []
+
+    total.addEventListener('click', ()=>{
+        let qtd_produto = document.getElementById('qtd_produto').value
+        let preco_produto = document.getElementById('preco_produto').value
+
+        qtd.push(qtd_produto)
+        preco.push(preco.produto)
+
+        let soma = 0.0  
+        for(i=0;i<preco.lenght;i++){
+            soma = soma + (qtd[i]*preco[i])
+        }
+        valor_total.innerHTML = ''
+            for(i=0;i<preco.lenght;i++){
+                    valor_total.innerHTML += 'Quantidade = ' +qtd[i] + '&emsp'
+                    valor_total.innerHTML += 'Preço Produto = ' +preco[i] + '&emsp'
+        }
+    })
+    
+
+
+    
+
+ 
+
+
